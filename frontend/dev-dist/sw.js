c88cb69787ec929e0bc8eb5289e9cd3f0ceb1680
eduardoklugqa <eduardo.klug7@gmail.com>
2025-11-13 19:54:35 -0300

    chore: remove obsolete files from the project
    - Deleted the `Aula08_Modelagem_Churn.ipynb` Jupyter notebook as it is no longer needed.
    - Removed the HTML file `Keeping a Render Backend Alive: What Worked, What Didn’t` from the project to streamline the repository and eliminate outdated content.
    - Updated the service worker file to reflect the changes in cached resources.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mq2jt3d8eig"
+    "revision": "0.od6oqunceo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
