79709ab7906fd74e596ad5b7f516a72c5bf7f3af
eduardoklugqa <eduardo.klug7@gmail.com>
2025-11-14 21:23:01 -0300

    fix: atualizar a revisão do arquivo index.html no service worker
    - Modificada a revisão do arquivo index.html para refletir a versão mais recente no cache do service worker.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.od6oqunceo8"
+    "revision": "0.k80ljjchkc8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
